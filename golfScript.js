e10c9eaa77f27d75a548aa37ec94bd02412e52c6
Regie09 <56932355+Regie09@users.noreply.github.com>
2020-06-05 03:31:38 -0400

    Update golfScript.js

--- golfScript.js
+++ golfScript.js
@@ -1,18 +1,22 @@
 // assign the table row for hole 1 to a variable
-  var elem = document.getElementById("1")
+// var elem = document.getElementById("1")
 //var elem
   //= document.getElementById("1");
 // display the number of children (all td elements)
-console.log(elem.children.length);
+//console.log(elem.children.length);
 // console.log(elem.children.length);
 // display the content of the + button, which is the first child of the fifth element
-console.log(elem.children[4].children[0]); 
+//console.log(elem.children[4].children[0]); 
 // assign a function to the + button
-elem.children[4].children[0].onclick 
+var x=0;
+function action (elem) {
+var elem
+  = document.getElementById(elem);
+elem.children[4].children[0].onclick
   = function(){add1(elem);};
 elem.children[4].children[1].onclick 
   = function(){subtract1(elem);};
-//Hole 2
+  }
 
 // you don't have to define the function before you use it! 
 function add1 (elem) {
