7faf330c84cfe2acedcf9c1e472892493093b0e7
Regie09 <56932355+Regie09@users.noreply.github.com>
2020-06-09 00:54:49 -0400

    Update golfScript.js

--- golfScript.js
+++ golfScript.js
@@ -1,70 +1,109 @@
-// assign the table row for hole 1 to a variable
-// var elem = document.getElementById("1")
-//var elem
-  //= document.getElementById("1");
-// display the number of children (all td elements)
-//console.log(elem.children.length);
-// console.log(elem.children.length);
-// display the content of the + button, which is the first child of the fifth element
-//console.log(elem.children[4].children[0]); 
-// assign a function to the + button
-var x=0;
-function action (elem) {
-var elem
-  = document.getElementById(elem);
-elem.children[4].children[0].onclick
-  = function(){add1(elem);};
-elem.children[4].children[1].onclick 
-  = function(){subtract1(elem);};
-  }
-
-// you don't have to define the function before you use it! 
-function add1(elem) {
-  let par = elem.children[1].innerHTML;
-  par = Number.parseInt(par);
-  x++;
-  if (elem.children[2].innerHTML == "-") {
-    elem.children[2].innerHTML = "1";
-    elem.children[3].innerHTML = 1 - par;
-  } else {
-    let currentScore = elem.children[2].innerHTML;
-    currentScore = Number.parseInt(currentScore);
-    elem.children[2].innerHTML = currentScore + 1;
-    elem.children[3].innerHTML = currentScore - par + 1;
-  }
-}
-
-//elem.children[2] is Score
-function subtract1 (elem) {
-  let par = elem.children[1].innerHTML;
-  par = Number.parseInt(par);
-  x--;
-  if(elem.children[2].innerHTML == "-") elem.children[2].innerHTML = "-1";
-    elem.children[3].innerHTML = -1 - par;
-  } else {
-    let currentScore = elem.children[2].innerHTML;
-    currentScore = Number.parseInt(currentScore);
-    elem.children[2].innerHTML = currentScore - 1;
-    elem.children[3].innerHTML = currentScore - par - 1;
-  }
-}
-  
- function calculate (elem) {
-  if(elem.children[3].innerHTML == "-") elem.children[3].innerHTML = "-1";
+function add1 (num) {
+var x = document.getElementById(num);
+  if(x.children[2].innerHTML == "-"){
+    x.children[2].innerHTML = "1";
+    x.children[3].innerHTML = "-3";
+  } 
   else {
-    let currentScore = elem.children[3].innerHTML;
+    let currentScore = x.children[2].innerHTML;
     currentScore = Number.parseInt(currentScore);
-    elem.children[3].innerHTML = currentScore - 4;
-    
+    x.children[2].innerHTML = currentScore + 1;
+    x.children[3].innerHTML = currentScore - 3;
   }
+  
+  var count = 1;
+  var all = 0;
+  var par = 0;
+  var score = 0;
+  var over = 0;
+  var curr = document.getElementById("1");
+  while(count <= 18){
+    curr = document.getElementById(count);
+    if(curr.children[2].innerHTML == "-"){
+    }
+    else{
+        all += 1;
+        par = par + Number.parseInt(curr.children[1].innerHTML);
+        score = score + Number.parseInt(curr.children[2].innerHTML);
+        over = over + Number.parseInt(curr.children[3].innerHTML);
+    }
+    count++;
+  }
+  
+  var z = document.getElementById("19");
+  z.children[0].innerHTML = all;
+  z.children[1].innerHTML = par;
+  z.children[2].innerHTML = score;
+  z.children[3].innerHTML = over;  
 }
 
-/* your mission: 
-1. make all the + buttons add 1 to the proper hole. 
-2. make all the - buttons subtract 1 from the proper hole.
-3. update the "Over" column to show the difference between par and score.
-4. update row #19 with appropriate totals. Note: if a person has not finished a round, then Over/Under should apply only to holes that have been updated.
-5. Add a new button, C, which clears the current score for a given hole
-6. advanced: put circles around birdies and squares around bogeys!
-*/
+function subtract1 (num2) {
+    var y = document.getElementById(num2)
+  if(y.children[2].innerHTML == "-"){
+     y.children[2].innerHTML = "-1";
+     y.children[3].innerHTML = "-5";
+  }
+  else {
+    let currentScore = y.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    y.children[2].innerHTML = currentScore - 1;
+    y.children[3].innerHTML = currentScore - 5;
+    }
+  
+  var count2 = 1;
+  var all2 = 0;
+  var par2= 0;
+  var score2 = 0;
+  var over2 = 0;
+  var currEl = document.getElementById("1");
+  while(count2 <= 18){
+    currEl = document.getElementById(count2);
+    if(currEl.children[2].innerHTML == "-"){
+    }
+    else{
+        all2 += 1;
+        par2 = par2 + Number.parseInt(currEl.children[1].innerHTML);
+        score2 = score2 + Number.parseInt(currEl.children[2].innerHTML);
+        over2 = over2 + Number.parseInt(currEl.children[3].innerHTML);
+    }
+    count2++;
+  }
+  
+  var j = document.getElementById("19");
+  j.children[0].innerHTML = all2;
+  j.children[1].innerHTML = par2;
+  j.children[2].innerHTML = score2;
+  j.children[3].innerHTML = over2;
+  }
 
+function clear (num3) {
+    console.log("invoked clear");
+var elem = document.getElementById(num3);
+elem.children[2].innerHTML = "-";
+elem.children[3].innerHTML = "-";
+
+  var count3 = 1;
+  var all3 = 0;
+  var par3= 0;
+  var score3 = 0;
+  var over3 = 0;
+  var currElem = document.getElementById("1");
+  while(count3 <= 18){
+    currElem = document.getElementById(count3);
+    if(currElem.children[2].innerHTML == "-"){
+
+    }
+    else{
+        all3 += 1;
+        par3 = par3 + Number.parseInt(currElem.children[1].innerHTML);
+        score3 = score3 + Number.parseInt(currElem.children[2].innerHTML);
+        over3 = over3 + Number.parseInt(currElem.children[3].innerHTML);
+    }
+    count3++;
+  }
+  var k = document.getElementById("19");
+  k.children[0].innerHTML = all3;
+  k.children[1].innerHTML = par3;
+  k.children[2].innerHTML = score3;
+  k.children[3].innerHTML = over3;
+}
